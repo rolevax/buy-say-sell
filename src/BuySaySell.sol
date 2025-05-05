f5482081ae3af5e8ce88fc0b4ab8744b00b3d611
rolevax <rolevax@outlook.com>
2025-05-05 23:57:03 +0800

    add offer buy/sell, agree

--- src/BuySaySell.sol
+++ src/BuySaySell.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
+import {console} from "forge-std/Test.sol";
+
 contract BuySaySell {
     Story[] private s_stories;
 
@@ -9,22 +11,107 @@ contract BuySaySell {
         string content;
     }
 
+    error UserArgError();
+    error OwnerError();
+    error NotSaidError();
+    error PriceError();
+    error TransferError();
+
     struct Story {
         uint256 index;
         address owner;
+        uint256 sellPrice;
+        uint256 buyPrice;
+        address buyer;
         Comment[] comments;
     }
 
     function createStory(string memory content) public {
         Story storage story = s_stories.push();
 
-        story.index = s_stories.length;
+        story.index = s_stories.length - 1;
         story.owner = msg.sender;
 
         story.comments.push(Comment({
             owner: msg.sender,
             content: content
         }));
+    }
+
+    function offerSellPrice(uint256 storyIndex, uint256 price) public {
+        if (price == 0) {
+            revert UserArgError();
+        }
+
+        if (storyIndex >= s_stories.length) {
+            revert UserArgError();
+        }
+
+        Story storage story = s_stories[storyIndex];
+        if (story.owner != msg.sender) {
+            revert OwnerError();
+        }
+
+        uint256 last = story.comments.length - 1;
+        if (story.comments[last].owner != msg.sender) {
+            revert NotSaidError();
+        }
+
+        story.sellPrice = price;
+    }
+
+    function offerBuyPrice(uint256 storyIndex, uint256 price) public {
+        if (price == 0) {
+            revert UserArgError();
+        }
+
+        if (storyIndex >= s_stories.length) {
+            revert UserArgError();
+        }
+
+        Story storage story = s_stories[storyIndex];
+        if (story.owner == msg.sender) {
+            revert OwnerError();
+        }
+
+        uint256 last = story.comments.length - 1;
+        if (story.comments[last].owner != story.owner) {
+            revert NotSaidError();
+        }
+
+        if (price <= story.buyPrice) {
+            revert PriceError();
+        }
+
+        story.buyPrice = price;
+        story.buyer = msg.sender;
+    }
+
+    function agreeSellPrice(uint256 storyIndex) public payable {
+        if (storyIndex >= s_stories.length) {
+            revert UserArgError();
+        }
+
+        Story storage story = s_stories[storyIndex];
+        address prevOwner = story.owner;
+        if (prevOwner == msg.sender) {
+            revert OwnerError();
+        }
+
+        uint256 price = story.sellPrice;
+        if (price == 0 || msg.value != price) {
+            revert PriceError();
+        }
+
+        (bool sent,) = prevOwner.call{value: price}("");
+        if (!sent) {
+            revert TransferError();
+        }
+
+        story.sellPrice = 0;
+        story.buyPrice = 0;
+        story.buyer = address(0);
+        story.owner = msg.sender;
     }
 
     function getStories() public view returns(Story[] memory) {
--- test/BuySaySell.t.sol
+++ test/BuySaySell.t.sol
@@ -7,22 +7,72 @@ import {BuySaySell} from "../src/BuySaySell.sol";
 contract BuySaySellTest is Test {
     BuySaySell public bss;
 
-    address USER = makeAddr("alice");
+    address USER1 = makeAddr("alice");
+    address USER2 = makeAddr("bob");
+    uint256 USER_BALANCE = 1 ether;
 
     function setUp() public {
         bss = new BuySaySell();
+        deal(USER1, USER_BALANCE);
+        deal(USER2, USER_BALANCE);
     }
 
     function test_createStory() public {
-        vm.prank(USER);
+        vm.prank(USER1);
 
         bss.createStory("aaa");
         BuySaySell.Story[] memory stories = bss.getStories();
         assertEq(stories.length, 1);
 
         BuySaySell.Story memory s = stories[0];
-        assertEq(s.owner, USER);
+        assertEq(s.owner, USER1);
         assertEq(s.comments[0].content, "aaa");
-        assertEq(s.comments[0].owner, USER);
+        assertEq(s.comments[0].owner, USER1);
+    }
+
+    function test_offerSell() public {
+        vm.startPrank(USER1);
+
+        bss.createStory("aaa");
+        bss.offerSellPrice(0, 123);
+
+        vm.stopPrank();
+
+        BuySaySell.Story[] memory stories = bss.getStories();
+        BuySaySell.Story memory s = stories[0];
+        assertEq(s.sellPrice, 123);
+    }
+
+    function test_offerBuy() public {
+        vm.prank(USER1);
+        bss.createStory("aaa");
+
+        vm.prank(USER2);
+        bss.offerBuyPrice(0, 123);
+
+        BuySaySell.Story[] memory stories = bss.getStories();
+        BuySaySell.Story memory s = stories[0];
+        assertEq(s.buyPrice, 123);
+        assertEq(s.buyer, USER2);
+    }
+
+    function test_agreeSell() public {
+        vm.prank(USER1);
+        bss.createStory("aaa");
+        vm.prank(USER1);
+        bss.offerSellPrice(0, 123);
+
+        vm.prank(USER2);
+        uint256 user1Balance1 = USER1.balance;
+        bss.agreeSellPrice{value: 123}(0);
+        uint256 user1Balance2 = USER1.balance;
+
+        BuySaySell.Story[] memory stories = bss.getStories();
+        BuySaySell.Story memory s = stories[0];
+        assertEq(s.owner, USER2);
+        assertEq(s.sellPrice, 0);
+        assertEq(s.buyPrice, 0);
+        assertEq(s.buyer, address(0));
+        assertEq(user1Balance2 - user1Balance1, 123);
     }
 }
